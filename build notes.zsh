2e0b0a32e1fec4fe58f1903c978eaf86051cf4fb
Amy <57969415+ASentientBot@users.noreply.github.com>
2023-01-16 12:26:37 -0500

    minor change build notes

--- build notes.zsh
+++ build notes.zsh
@@ -2,38 +2,30 @@
 
 # https://airdownload.adobe.com/air/mac/download/32.0/AIRSDK_Compiler.dmg
 airNative="$PWD/Downloads/AIRMac"
-
 # https://airdownload.adobe.com/air/win/download/32.0/AIRSDK_Compiler.zip
 airWindows="$PWD/Downloads/AIRWindows"
-
 # https://download.oracle.com/java/19/latest/jdk-19_windows-x64_bin.zip
 jdkWindows="$PWD/Downloads/JDKWindows"
-
 # https://github.com/pixeljam/DinoRunDX/archive/refs/heads/main.zip
 dr="$PWD/Downloads/DRDX"
-
 # https://sourceforge.net/projects/box2dflash/files/box2dflash/Box2DFlashAS3_2.1a/Box2DFlashAS3%202.1a.zip/download
 b2d="$PWD/Downloads/Box2D"
-
 # https://github.com/Gamua/Starling-Framework/releases/download/v2.7/starling-2.7.zip
 starling="$PWD/Downloads/Starling"
-
 # https://dump.ventero.de/FRESteamWorks/v0.7/FRESteamWorks.ane
 steamWrapper="$PWD/Downloads/FRESteamWorks.ane"
-
 # https://partner.steamgames.com/downloads/steamworks_sdk.zip
 steam="$PWD/Downloads/Steam"
-
-password=correcthorsebatterystaple
+PATH+=:"/Applications/CrossOver.app/Contents/SharedSupport/CrossOver/bin:$airNative/bin"
 
 rm -rf Temp
 mkdir Temp
 cd Temp
 
-PATH+=:"/Applications/CrossOver.app/Contents/SharedSupport/CrossOver/bin:$airNative/bin"
-
 # cxbottle --create --template win10_64 --bottle DRDX
 bottle=DRDX
+
+# TODO: figure out building SWCs (paid Flash IDE)
 
 # SWF
 amxmlc "$dr/src/base/Brain.as" -compiler.source-path+="$dr/src" -compiler.source-path+="$b2d/Source" -compiler.library-path+="$starling/starling/bin/starling.swc" -compiler.library-path+="$dr/assets/swcs/DR_Audio.swc" -compiler.library-path+="$dr/assets/swcs/DR_Interface.swc" -compiler.library-path+="$dr/assets/swcs/DR_Main.swc" -compiler.library-path+="$dr/assets/swcs/DR_Nodes.swc" -external-library-path+="$steamWrapper" -output Brain.swf
@@ -45,7 +37,6 @@ cp -R "$dr/src/assets" .
 cp "$steamWrapper" .
 cp "$steam/redistributable_bin/osx/libsteam_api.dylib" .
 cp "$steam/redistributable_bin/win64/steam_api64.dll" .
-
 mkdir Unpacked
 unzip "$steamWrapper" -d Unpacked/SteamWrapper.ane
 echo 248330 > steam_appid.txt
@@ -57,6 +48,7 @@ DYLD_LIBRARY_PATH="$steam/redistributable_bin/osx" adl -profile extendedDesktop 
 wine --bottle "$bottle" "$airWindows/bin/adl.exe" -profile extendedDesktop -extdir Unpacked App.xml
 
 # certificate
+password=correcthorsebatterystaple
 adt -certificate -cn Amy 2048-RSA Cert.p12 "$password"
 
 # build macOS
